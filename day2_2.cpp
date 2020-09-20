179aefa7f246d1226e772a309e22c385d54c51f4
Pablo González Portela <pablo.portela@gmail.com>
2020-09-20 22:55:24 +0200

    Day 2, second part

--- day2_2.cpp
+++ day2_2.cpp
@@ -9,12 +9,13 @@ using namespace std;
 
 vector<int> parse_csv_ints(const char *filename) {
     fstream file;
-    string tp;
-    vector<int> integers;
     file.open(filename);
     if (!file.is_open()) {
         throw runtime_error(string("Unable to open file ") + string(filename));
     }
+
+    string tp;
+    vector<int> integers;
     while (getline(file, tp, ',')) {
         integers.push_back(stoi(tp));
     }
@@ -33,7 +34,7 @@ void print_intcode_program(vector<int> &intcode_program) {
 
 
 void run_intcode_program(vector<int> &p) {
-    
+
     for (size_t i{}; i<p.size(); i+=4) {
 
         switch (p[i]) {
@@ -55,14 +56,30 @@ void run_intcode_program(vector<int> &p) {
 }
 
 
+int get_answer(vector<int> &p) {
+    for (int noun{}; noun<100; noun++) {
+        for (int verb{}; verb<100; verb++) {
+            // reset the memory
+            vector<int> p_copy = p;
+
+            // try noun and verb
+            p_copy[1] = noun;
+            p_copy[2] = verb;
+            run_intcode_program(p_copy);
+
+            // found it
+            if (p_copy[0] == 19690720) {
+                return 100 * noun + verb;
+            }
+        }
+    }
+
+    throw runtime_error("Unable to find the answer");
+}
+
+
 int main(int argc, char **argv) {
     vector<int> intcode_program = parse_csv_ints(argv[1]);
-
-    intcode_program[1] = 12;
-    intcode_program[2] = 2;
-    run_intcode_program(intcode_program);
-
-    cout << "First position after running: " << intcode_program[0] << endl;
-
+    cout << "And the winner is: " << get_answer(intcode_program) << endl;
     return 0;
 }
